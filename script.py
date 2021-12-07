9327785044e238b56ffd6a84a2c5fc0dac548046
RadimJedlicka <radim.jedi@gmail.com>
2021-12-07 13:55:40 +0100

    Added script.py

--- script.py
+++ script.py
@@ -1,3 +1,3 @@
 print("Hello world")
-I will test my knowledge on this repo.
-I have added new text into this file      # Pridavame tento radek.
+# I will test my knowledge on this repo.
+# I have added new text into this file      # Pridavame tento radek.
